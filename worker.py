330f716666f57ec7ecbd669bcc73ae30fac09e25
cliu <cliu0507@yahoo.com>
2020-09-29 16:31:37 -0700

    update queue name and attribute in message

--- worker.py
+++ worker.py
@@ -7,11 +7,11 @@ def main():
     workflow = WorkflowDeeplabV3()
 
     # listen to message queue
-    queue_name = 'workflow1'
+    queue_name = 'demo-queue'
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     channel.queue_declare(queue=queue_name)
-    channel.basic_consume(queue='workflow1',
+    channel.basic_consume(queue=queue_name,
                           on_message_callback=lambda ch, method, properties, body: my_callback_with_extended_args(ch,method,properties,body,workflow=workflow),
                           auto_ack=True)
     print(' [*] Waiting for messages. To exit press CTRL+C')
@@ -22,9 +22,9 @@ def my_callback_with_extended_args(ch, method, properties, body, workflow):
     print(" [x] Received %r" % body.decode())
     # convert str to json
     body_dict = eval(body.decode())
-    print(" [x] Processing %r " % body_dict['filepath'])
+    print(" [x] Processing %r " % body_dict['filePath'])
     output_filepath = './tmp/result.png'
-    workflow.start(input_filepath=body_dict['filepath'], output_filepath=output_filepath)
+    workflow.start(input_filepath=body_dict['filePath'], output_filepath=output_filepath)
     print(" [x] Result dumped into %r " % output_filepath)
     print(' [*] Waiting for messages. To exit press CTRL+C')
 
