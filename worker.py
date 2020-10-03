54e08442c2575339c8bacd9676e4fb8cb5097325
cliu <cliu0507@yahoo.com>
2020-10-02 23:05:50 -0700

    add try catch to worker

--- worker.py
+++ worker.py
@@ -46,44 +46,49 @@ def my_callback_with_extended_args(ch, method, properties, body, workflow, db):
     job_id = body_dict['jobId']  # this is the unique job id
     print(" [x] start to process job %r " % job_id)
 
-    # pull status from database
-    print(" [x] pull job status from database")
-    query = {"jobId": job_id}
-    query_result = db[collection_name].find_one(query)
-    file_id = query_result['fileId']  # this is the object id of file on GridFS
-    new_file_name = query_result['newFileName']
-    mime_type = query_result['mimeType']
+    try:
+        # pull status from database
+        print(" [x] pull job status from database")
+        query = {"jobId": job_id}
+        query_result = db[collection_name].find_one(query)
+        file_id = query_result['fileId']  # this is the object id of file on GridFS
+        new_file_name = query_result['newFileName']
+        mime_type = query_result['mimeType']
 
-    # prepare temporary folder
-    output_folder_path = os.path.join('./tmp', job_id)
-    os.makedirs(output_folder_path, exist_ok=True)
+        # prepare temporary folder
+        output_folder_path = os.path.join('./tmp', job_id)
+        os.makedirs(output_folder_path, exist_ok=True)
 
-    # download input file to tmp folder
-    print(' [x] download file from GridFS %r' % new_file_name)
-    fs = gridfs.GridFS(db)
-    f_like_object = fs.get(file_id=ObjectId(file_id))
-    new_file_name_path = os.path.join(output_folder_path, new_file_name)
-    f = open(new_file_name_path, 'wb')
-    f.write(f_like_object.read())
-    f.close()
-    output_filepath = os.path.join(output_folder_path, 'result.png')
+        # download input file to tmp folder
+        print(' [x] download file from GridFS %r' % new_file_name)
+        fs = gridfs.GridFS(db)
+        f_like_object = fs.get(file_id=ObjectId(file_id))
+        new_file_name_path = os.path.join(output_folder_path, new_file_name)
+        f = open(new_file_name_path, 'wb')
+        f.write(f_like_object.read())
+        f.close()
+        output_filepath = os.path.join(output_folder_path, 'result.png')
 
-    # update status to 'processing' in database
-    print(" [x] update job status to database")
-    new_query = {"jobId": job_id}
-    new_value = {"$set": {"jobStatus": "processing"}}
-    db[collection_name].update_one(new_query, new_value)
+        # update status to 'processing' in database
+        print(" [x] update job status to database")
+        new_query = {"jobId": job_id}
+        new_value = {"$set": {"jobStatus": "processing"}}
+        db[collection_name].update_one(new_query, new_value)
 
-    # kick off the business logic workflow
-    workflow.start(input_filepath=new_file_name_path, output_filepath=output_filepath)
-    print(" [x] complete! result dumped into %r " % output_filepath)
-    print(' [*] waiting for messages. To exit press CTRL+C')
+        # kick off the business logic workflow
+        workflow.start(input_filepath=new_file_name_path, output_filepath=output_filepath)
+        print(" [x] complete! result dumped into %r " % output_filepath)
 
-    # update status to 'completed' in database
-    print(" [x] update job status to database")
-    new_query = {"jobId": job_id}
-    new_value = {"$set": {"jobStatus": "completed"}}
-    db[collection_name].update_one(new_query, new_value)
+        # update status to 'completed' in database
+        print(" [x] update job status to database")
+        new_query = {"jobId": job_id}
+        new_value = {"$set": {"jobStatus": "completed"}}
+        db[collection_name].update_one(new_query, new_value)
+        print(' [*] waiting for messages. To exit press CTRL+C')
+    except:
+        print(" [x] exception! --- job id %s" % job_id)
+        print(' [*] waiting for messages. To exit press CTRL+C')
+
 
 if __name__ == '__main__':
     main()
